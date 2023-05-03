ad4f777cd16b3d659fd0785a3fbb51e4bc96fc14
Grevalby <117574329+grevalby@users.noreply.github.com>
2023-05-03 10:59:54 +0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+/* const li = document.querySelectorAll(".nav-item");
+const artic = document.querySelectorAll(".card");
+
+function activeMenu() {
+  let len = artic.lenght;
+  while (--len && window.scrollY + 97 < artic[len].offsetTop) {}
+  li.forEach((ltx) => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll".activeMenu); */
